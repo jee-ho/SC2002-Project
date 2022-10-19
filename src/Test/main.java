78fc70ac861062c4da641c1c84bd09593455faef
LECAGO <lecago@pm.me>
2022-10-19 23:25:25 +0800

    added javadoc

--- src/Test/main.java
+++ src/Test/main.java
@@ -1,4 +1,15 @@
+package Test;
+/**
+ * This is the main class.
+ * @author Tan Chuan Liang
+ * @version 1.0
+ * @since 2022-10-19
+ */
 class Main {
+	/**
+	 * This is the main() function.
+	 * @param args The arguments passed into main.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 
